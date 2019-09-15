81824a54bba1c01b49dc5f73e3ed7d1a4c20384b
Adam Janca <janca.adam89@gmail.com>
2019-09-15 15:23:39 +0200

    models added

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# TODO - downnload test_module.py and test your app
 def index(request):
-	return HttpResponse("Rango says hey there partner! Go to about page: "
-		"<a href='/rango/about'>about</a>")
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page. Go back: "
-		"<a href='/rango'>index</a>")
+	context_dict = {'your_name' :'Adam Janca'}
+	return render(request, 'rango/about.html', context=context_dict)
